90e7d370d55c38a10e2fd919c356a0fa7e80b862
mpchambe <mpchambe@ad3.ucdavis.edu@pc15.cs.ucdavis.edu>
2021-05-20 17:19:44 -0700

    passes 100%

--- hw4-handout/bug2/bug2.go
+++ hw4-handout/bug2/bug2.go
@@ -6,10 +6,15 @@ import (
 
 func bug2(n int, foo func(int) int, out chan int) {
 	var wg sync.WaitGroup
+	// make a channel of size n
+	ch := make(chan int, n)
 	for i := 0; i < n; i++ {
+		// put i into the channel
+		ch <- i
 		wg.Add(1)
 		go func() {
-			out <- foo(i)
+			// output the channel contents 
+			out <- foo(<-ch)
 			wg.Done()
 		}()
 	}
